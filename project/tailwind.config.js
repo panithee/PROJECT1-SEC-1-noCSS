2a66a5056e1dad3c68bf7e91a0492888070ca885
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-02-04 16:18:12 +0700

    custome color and font-family

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        itim: "'Itim', cursive"
+      },
+
+      colors: {
+        "brownFont": '#5D3739',
+        "btnFont": '#F7D0BE',
+        "bgPage": '#F7D0BE',
+        "bgBox": '#FFF7F0',
+        "bgList1": '#FFE8D3',
+        "bgList2": '#ECFFE9 ',
+        "bgFooter": '#FFE8D3',
+        "btn1": '#DD743E',
+        "checkList1": '#D9D9D9',
+        "checkList2": '#3C6673'
+
+      }
+    },
   },
   plugins: [],
 }
