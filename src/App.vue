023c09dd778cc28bf566d68570835fed4b1833bf
Chalunthorn_niyom <Chalunthorn.niyom@gmail.com>
2023-02-18 15:15:14 +0700

    update beta

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,17 @@
 <script setup>
-import { ref, toRef, toRefs, computed } from "vue";
+import { ref, computed } from "vue";
 
 let persons = ref([
   { name: "John", price: 200, status: false },
-  { name: "jhon", price: 200, status: false },
   { name: "Prim", price: 200, status: false },
-  { name: "Henry", price: 200, status: false },
-  { name: "Mo", price: 200, status: false },
-  { name: "Eve", price: 200, status: false },
-  { name: "Oat", price: 200, status: false },
-  { name: "Henry", price: 200, status: false },
-  { name: "Mo", price: 200, status: false },
-  { name: "Eve", price: 200, status: false },
-  { name: "Oat", price: 200, status: false },
-  { name: "Henry", price: 200, status: false },
-  { name: "Mo", price: 200, status: false },
-  { name: "Eve", price: 200, status: false },
-  { name: "Oat", price: 200, status: false },
 ]);
 
 let foodLists = ref([
-  { name: "somtum", price: 2020, person: [persons.value[0]] },
+  { name: "somtum", price: 2020, person: [] },
   { name: "banana", price: 200, person: [persons.value[0], persons.value[1]] },
-  { name: "Kaiped", price: 200, person: [persons.value[0]] },
-  { name: "Kaikai", price: 200, person: [persons.value[0], persons.value[1]] },
-  { name: "somtum", price: 200, person: [persons.value[0]] },
-  { name: "banana", price: 200, person: [persons.value[0], persons.value[1]] },
-  { name: "Kaiped", price: 200, person: [persons.value[0]] },
-  { name: "Kaikai", price: 200, person: [persons.value[0], persons.value[1]] },
-  { name: "somtum", price: 200, person: [persons.value[0]] },
-  { name: "banana", price: 200, person: [persons.value[0], persons.value[1]] },
-  { name: "Kaiped", price: 200, person: [persons.value[0]] },
-  { name: "Kaikai", price: 200, person: [persons.value[0], persons.value[1]] },
 ]);
 
-
-
-const clearFoodList = () => {
-  foodLists.value = [];
-  return foodLists;
-};
-
-const deleteAll = () => {
-  persons.value = [];
-  return persons;
-};
+// switch page
 const sw = ref(true);
 const switchMenu = (e) => {
   if (e === "list") {
@@ -54,9 +21,8 @@ const switchMenu = (e) => {
     sw.value = false;
   }
 };
-const totalFoodLits = ref(
-  foodLists.value.reduce((total, food) => total + food.price, 0)
-);
+
+// List food
 let price = ref(0);
 let foodName = ref("");
 let personFood = ref([]);
@@ -65,21 +31,21 @@ let target = ref("");
 let personSize = ref();
 
 const eventFoodList = (e, mode) => {
-  target.value = e.target.id;
+
   if (mode === "add") {
     modeTarget.value = "add";
-    personSize = 0 //try
     price.value = 0;
     foodName.value = "";
     personFood.value = [];
   }
   if (mode === "edit") {
+    target.value = e.target.id;
     modeTarget.value = "edit";
-    price.value = foodLists.value[e.target.id].price;
-    foodName.value = foodLists.value[e.target.id].name;
-    personFood.value = foodLists.value[e.target.id].person;
-    personSize = foodLists.value[e.target.id].person.length;
-    // foodLists.value[e.index] = e.food;
+    const food = foodLists.value[e.target.id];
+    price.value = food.price;
+    foodName.value = food.name;
+    personFood.value = food.person;
+    personSize = food.person.length;
   } else {
     console.log("Error");
   }
@@ -87,19 +53,24 @@ const eventFoodList = (e, mode) => {
 };
 
 const doneBtn = () => {
-  if (modeTarget.value === "add") {
+  if (foodName.value === "" || price.value === " ") {
+    console.log("Error");
+  }
+  else if (modeTarget.value === "add") {
     foodLists.value.push({
       name: foodName.value,
       price: price.value,
       person: personFood.value,
     });
+    foodName.value = "";
+    price.value = 0;
+    personFood.value = [];
   } else if (modeTarget.value === "edit") {
 
     foodLists.value[target.value].price = price.value;
     foodLists.value[target.value].name = foodName.value;
     foodLists.value[target.value].person = personFood.value;
-  }
-  else {
+  } else {
     console.log("Error");
   }
   console.log(foodLists.value[target]);
@@ -110,20 +81,77 @@ const doneBtn = () => {
 
   showMenu();
 };
+const clearFoodList = () => {
+  foodLists.value = [];
+  return foodLists;
+};
+
+const deleteAll = () => {
+  persons.value = [];
+  return persons;
+};
 
 const deleteBtn = () => {
-  foodLists.value.splice(target.value, 1)
+  foodLists.value.splice(parseInt(target.value), 1)
   showMenu();
 }
-
+const totalFoodLists = computed(() => foodLists.value.reduce((total, food) => total + food.price, 0));
+// show model
 const showMenuStatus = ref(false);
 const showMenu = () => {
   console.log("showMenu");
   showMenuStatus.value = !showMenuStatus.value;
 };
 
+const checkPerson = (personName) => {
+  return personFood.value.filter(person => person.name === personName).length > 0;
+};
 
+const chooseToggle = (personName) => {
+  if (checkPerson(personName)) {
+    personFood.value = personFood.value.filter(
+      (person) => person.name !== personName
+    );
+  } else {
+    personFood.value.push(persons.value.find((person) => person.name === personName));
+  }
+};
 
+const namePerson = ref("");
+const alertName = ref(false);
+const addPerson = () => {
+
+  if (namePerson.value !== "" && !(persons.value.find((person) => person.name.toLowerCase() === namePerson.value.toLowerCase()))) {
+    alertName.value = false;
+    persons.value.push({ name: namePerson.value, price: 0, status: false });
+
+    namePerson.value = "";
+  }
+  else {
+    alertName.value = true;
+  }
+};
+const deletePerson = (event) => {
+
+  for (const food of foodLists.value) {
+    food.person = food.person.filter((person) => person.name !== persons.value[event.target.id].name);
+  }
+  persons.value.splice(event.target.id, 1);
+};
+// person
+const personPrices = computed(() => {
+  for (const person of persons.value) {
+    person.price = 0;
+    for (const food of foodLists.value) {
+      for (const personFood of food.person) {
+        if (person.name === personFood.name) {
+          person.price += food.price / food.person.length;
+        }
+      }
+    }
+  }
+  return "";
+});
 
 </script>
 
@@ -135,7 +163,7 @@ const showMenu = () => {
       </h1>
 
       <!-- Switch btn page -->
-      <div class="flex items-center w-1/2 m-auto mt-10 rounded-full bg-bgbtn" v-if="sw">
+      <div v-if="sw" class="flex items-center w-1/2 m-auto mt-10 rounded-full bg-bgbtn">
         <div class="flex justify-center w-1/2 bg-btn1 rounded-full my-1 ml-1 mr-0.5" @click="switchMenu('list')">
           <button class="text-xl text-white">All List</button>
         </div>
@@ -143,7 +171,7 @@ const showMenu = () => {
           <button class="text-xl text-brownFont">Per Person</button>
         </div>
       </div>
-      <div class="flex items-center w-1/2 m-auto mt-10 rounded-full bg-bgbtn" v-else>
+      <div v-else class="flex items-center w-1/2 m-auto mt-10 rounded-full bg-bgbtn">
         <div class="flex justify-center w-1/2 rounded-full my-1 ml-1 mr-0.5 cursor-pointer" @click="switchMenu('list')">
           <button class="text-xl text-brownFont">All List</button>
         </div>
@@ -153,7 +181,7 @@ const showMenu = () => {
         </div>
       </div>
 
-      <div class="w-5/6 m-auto mt-10 overflow-y-scroll rounded-3xl bg-bgBox sm:w-1/2 h-1/2" v-if="sw">
+      <div v-if="sw" class="w-5/6 m-auto mt-10 overflow-y-scroll rounded-3xl bg-bgBox sm:w-1/2 h-1/2">
         <table class="w-full text-brownFont">
           <thead>
             <tr>
@@ -175,7 +203,7 @@ const showMenu = () => {
                 <div
                   class="flex flex-wrap w-24 h-20 overflow-y-scroll sm:overflow-hidden sm:flex-nowrap sm:w-36 sm:h-auto sm:overflow-x-scroll">
                   <div v-for="(person, index) in foodLists[index].person" key="index">
-                    <span class="px-1 ml-1 text-base" :class="['color-' + (index % 4)]">{{ person.name }}</span>
+                    <span :class="['color-' + (index % 4)]" class="px-1 ml-1 text-base">{{ person.name }}</span>
                   </div>
                 </div>
 
@@ -185,7 +213,7 @@ const showMenu = () => {
                 {{ food.price / food.person.length }}
               </td>
               <td>
-                <img alt="iconEdit" :id="index" class="w-5 h-5 mr-3" src="./assets/more-vertical.svg"
+                <img :id="index" alt="iconEdit" class="w-5 h-5 mr-3" src="./assets/more-vertical.svg"
                   @click="eventFoodList($event, 'edit')" />
               </td>
             </tr>
@@ -203,7 +231,7 @@ const showMenu = () => {
         </button>
       </div>
 
-      <div class="w-5/6 m-auto mt-10 overflow-y-scroll rounded-3xl bg-bgBox sm:w-1/2 h-1/2" v-if="!sw">
+      <div v-if="!sw" class="w-5/6 m-auto mt-10 overflow-y-scroll rounded-3xl bg-bgBox sm:w-1/2 h-1/2">
         <table class="w-full text-brownFont">
           <thead>
             <tr>
@@ -216,25 +244,27 @@ const showMenu = () => {
             <tr v-for="(person, index) in persons" key="index" :class="person.status ? 'bg-checkboxGreen' : 'bg-bgBox'">
               <td class="pl-16 text-2xl text-left">
                 <div class="mt-2">
-                  <input type="checkbox" v-model="person.status" />
+                  <input v-model="person.status" type="checkbox" />
                   <label class="= text-2xl"></label>
                   {{ person.name }}
                 </div>
               </td>
+              {{ personPrices }}
               <td class="text-2xl text-center">{{ person.price }}</td>
+              <td> <img alt="" :id="index" class="w-5 h-10" src="./assets/x.svg" @click="deletePerson($event)" /></td>
             </tr>
           </tbody>
         </table>
       </div>
 
-      <div @click="deleteAll" class="flex items-center justify-center w-2/12 h-12 m-auto mt-5 rounded-full bg-btn1"
-        v-if="!sw">
+      <div v-if="!sw" class="flex items-center justify-center w-2/12 h-12 m-auto mt-5 rounded-full bg-btn1"
+        @click="deleteAll">
         <button class="text-xl text-center text-white">Delete All</button>
       </div>
       <div class="fixed bottom-0 w-full h-20 bg-bgFooter">
         <div class="flex">
           <p class="w-1/2 text-2xl text-center pt-7">
-            Total:{{ totalFoodLits }}
+            Total:{{ totalFoodLists }}
           </p>
           <p class="w-1/2 text-2xl text-center pt-7">
             Person: {{ persons.length }}
@@ -245,22 +275,22 @@ const showMenu = () => {
     </div>
 
     <!-- Model -->
-    <div class="fixed w-full h-full" v-show="showMenuStatus">
+    <div v-show="showMenuStatus" class="fixed w-full h-full">
       <!-- <div class="absolute inset-0 bg-zinc-500/50 " @click="showMenu()"> -->
       <div class="flex items-center justify-center h-screen bg-[#FFF7F0] bg-opacity-70">
         <div class="lg"></div>
         <div class="flex flex-col w-[776px] h-[448px] bg-bgPage rounded-[40px]">
 
           <div class="flex w-full h-[44px] justify-end">
-            <img alt="" class="w-10 h-10 mt-4 mr-4" @click="showMenu()" src="./assets/x.svg" />
+            <img alt="" class="w-10 h-10 mt-4 mr-4" src="./assets/x.svg" @click="showMenu()" />
           </div>
 
           <div class="lg:flex lg:max-w-3xl -">
             <div class="flex pl-14 pr-7">
               <div class="flex flex-col items-center rounded-full bg-bgList1 w-[304px] h-[304px]">
                 <div class="flex justify-center items-center h-[168px] w-[296px] rounded-t-full bg-bgList1">
-                  <input type="number"
-                    class="h-[48px] w-[200px] text-center mt-12 text-[48px] bg-bgList1 text-brownFont" />
+                  <input class="h-[48px] w-[200px] text-center mt-12 text-[48px] bg-bgList1 text-brownFont" type="number"
+                    v-model="price" />
                 </div>
                 <div class="mt-3 w-[240px] border-b border-brownFont"></div>
                 <div></div>
@@ -272,51 +302,46 @@ const showMenu = () => {
               <div class="text-2xl first-letter:flex text-brownFont">
                 Your Food
               </div>
-              <input placeholder="Your Food" v-model="foodName"
-                class="mt-2 bg-bgbtn w-[304px] h-[48px] rounded-[20px] pl-4 text-[24px] text-brownFont" type="text" />
+              <input v-model="foodName"
+                class="mt-2 bg-bgbtn w-[304px] h-[48px] rounded-[20px] pl-4 text-[24px] text-brownFont"
+                placeholder="Your Food" type="text" />
               <div class="flex mt-2 text-2xl text-brownFont">Add Payer</div>
               <div class="flex mt-2">
                 <img alt="iconEdit" class="h-7" src="./assets/PersonCount.svg" /><span
                   class="pl-2 pr-3 text-xl bg-bgbtn rounded-tr-xl rounded-br-xl">{{ personSize }}</span>
               </div>
-
-              <!-- <div class="flex ">
-                                                              <img alt="iconEdit" class="w-10 h-10 mr-1" src="./assets/user-circle (3).svg" />
-                                                            </div> -->
-
               <div
                 :class="personFood.length !== 0 ? 'flex mt-2 bg-bgbtn w-[280px] h-[92px] rounded-2xl' : 'flex mt-2 bg-bgbtn w-[280px] h-[92px] rounded-2xl justify-center items-center'">
-                <div v-if="personFood.length == 0" class="flex items-center justify-center px-2 m-2 h-9 text-slate-700">No
-                  one right now~~</div>
+                <div v-if="persons.length === 0" class="flex items-center justify-center px-2 m-2 h-9 text-slate-700">
+                  No
+                  one right now~~
+                </div>
                 <div v-else
-                  class="flex flex-wrap w-24 h-20 overflow-y-scroll sm:overflow-hidden sm:flex-wrap sm:w-full sm:h-auto sm:overflow-y-scroll">
-                  <div class="mt-1 ml-1" v-for="(person, index) in persons" key="index">
-                    <span class="px-2 text-base " :class="['color-' + (index % 4)]">{{ person.name }}</span>
+                  class="flex flex-wrap w-24 h-20 overflow-y-scroll sm:overflow-hidden sm:flex-wrap sm:w-full sm:overflow-y-scroll">
+                  <div v-for="(person, index) in persons" key="index" class="mt-1 ml-1">
+                    <span :class="checkPerson(person.name) ? 'bg-btn1' : 'bg-bgPage'" class="px-2 text-base rounded-xl "
+                      @click="chooseToggle(person.name)">{{
+                        person.name
+                      }}</span>
                   </div>
                 </div>
               </div>
 
               <div class="flex flex-row mt-4">
                 <input class="h-8 w-[224px] pl-4 text-xl bg-bgbtn rounded-xl text-brownFont"
-                  placeholder="Please add name..." type="text" />
-                <button @click="addPerson"><img class="h-8 ml-3" src="./assets/addPersonBtn.svg" alt="" /></button>
+                  placeholder="Please add name..." type="text" v-model="namePerson" />
+                <button @click="addPerson"><img alt="" class="h-8 ml-3" src="./assets/addPersonBtn.svg" /></button>
               </div>
-              error
+              <strong class="text-red-600" v-show="alertName"> User already exists</strong>
+
               <div class="flex flex-row mt-1">
                 <button class="h-[56px] w-[106px] bg-btn1 rounded-3xl text-white text-xl" @click="doneBtn">
                   Done
                 </button>
                 <button class="ml-5 text-xl underline text-btn1" @click="deleteBtn">Delete</button>
               </div>
-
-              <!-- <img alt="iconEdit" class="w-10 h-10 mr-1" src="./assets/user-circle (3).svg" /> -->
-
-              <!-- <div class="flex justify-center">
-                                                              <button class="w-1/4 h-16 my-4 text-xl bg-white rounded-3xl text-btn1">Done</button>
-                                                            </div> -->
             </div>
           </div>
-          <!--         <div class=""></div>-->
         </div>
       </div>
     </div>
@@ -324,8 +349,6 @@ const showMenu = () => {
 </template>
 
 <style scoped>
-@import url(https://fonts.googleapis.com/css2?family=Itim&display=swap);
-
 h1,
 p,
 tr,
@@ -333,7 +356,7 @@ th,
 td,
 button,
 div {
-  font-family: "Itim";
+  font-family: "Itim", serif;
 }
 
 .color-0 {
