fffe5938e536d2b5ba5f1b8cb18af73303889cd0
panithee <panithee.pnt@mail.kmutt.ac.th>
2023-02-23 22:51:41 +0700

    change filter to find at checkPerson

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,7 @@ const doneBtn = () => {
 };
 
 const checkPerson = (personName) => {
-  return personFood.value.filter(person => person.name === personName).length > 0;
+  return personFood.value.find(person => person.name === personName);
 };
 
 const chooseToggle = (personName) => {
